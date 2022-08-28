42af6abfadb04d63b94a677cad6d37d59cead246
Yuri Hallan <yuri.s@samsung.com>
2022-08-28 19:34:01 -0300

    Add new file to generator a sticker from image top250

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -6,11 +8,9 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
-
-
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         // fazer uma conexão HTTP e Buscar os top 250 filmes
         String url = "https://imdb-api.com/en/API/Top250Movies/k_kyty6or8";
         URI endereco = URI.create(url);
@@ -18,18 +18,25 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        
 
         // extrair só os dados que interessam (titulo poster, classificação)
         JsonParser parser = new JsonParser();
         List<Map<String, String>> listDeFilmes = parser.parse(body);
 
-        //exibir e manipular os dados
-       for (Map<String,String> filme : listDeFilmes) {
+        // exibir e manipular os dados
+        var geradora = new GeradoraDeFigurinhas();
+        for (Map<String, String> filme : listDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
             System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRatingCount"));
             System.out.println();
-       }
+        }
     }
 }
---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+    
+    private static final Pattern REGEX_ITEMS_IMAGE = Pattern.compile(".\\([@][.])([\S]*)([.][a-z]{3})$");
+    
+    void cria(InputStream inputStream, String nomeArquivo) throws Exception{
+        //leitura da imagem
+        //InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@.jpg").openStream();
+
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        //cria nova imagem em memoria com transparência e com tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        //copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal,  0, 0, null);
+
+        //configurar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+        //escrever uma frase na nova imagem
+        graphics.drawString("TOPZEIRA", 0, novaAltura - 100);
+        
+
+       
+        
+
+        //escrever a nova em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+    }
+
+
+
+
+}
